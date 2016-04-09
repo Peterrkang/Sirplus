58987376a97af9c38758dd1d139ababc427c94b1
Mark Janzer <email": null>
2016-04-09 12:17:30 -0700

    add controllers

---
+++ app/controllers/providers_controller.rb
@@ -0,0 +1,22 @@
+class ProvidersController < ApplicationController
+  def show
+  end
+
+  def index
+  end
+
+  def create
+  end
+
+  def delete
+  end
+
+  def update
+  end
+
+  def edit
+  end
+
+  def new
+  end
+end
