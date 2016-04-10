a28c9cbd04de29ec7f970cc009e17fae63ecd48e
Chunyu Ou(欧春雨) <ouchunyu@yahoo.com>
2016-04-09 18:54:10 -0700

    Add user table

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,15 +1,16 @@
 class SessionsController < ApplicationController
 
-  before_action :logged_in_as_correct_user
+  # before_action :logged_in_as_correct_user
 
   def new
   end
 
   def create
-    @provider = Provider.find_by(email: params[:session][:email].downcase)
+    @user = User.find_by(email: params[:session][:email].downcase)
 
-    if @provider && @provider.authenticate(params[:session][:password])
-        log_in @provider
+    if @user && @user.authenticate(params[:session][:password])
+      log_in @user
+      redirect_to root_url
     else
       flash.now[:danger] = 'Wrong email address or password.'
       render 'new'
@@ -22,5 +23,13 @@ class SessionsController < ApplicationController
   end
 
 
+  private
+  def log_in(user)
+    session[:user_id] = user.id
+  end
+
+  def log_out
+    session[:user_id] = nil
+  end
 
 end
--- app/models/shelter.rb
+++ app/models/shelter.rb
@@ -1,13 +1,3 @@
-class Shelter < ActiveRecord::Base
-
-  before_save {self.email.downcase!}
-  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
-
-  validates :email, presence: true, length:     { maximum: 150 },
-                                    format:     { with: VALID_EMAIL_REGEX },
-                                    uniqueness: { case_sensitive: false }
-
-  has_secure_password
-  validates :password, presence: true, length: { minimum: 6 }
+class Shelter < User
 
 end
