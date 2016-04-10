1492d1bed8c3bdc6cfd11f43df10d05a71812988
Chunyu Ou(欧春雨) <ouchunyu@yahoo.com>
2016-04-09 20:47:07 -0700

    Finish

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -2,7 +2,7 @@ module SessionsHelper
 
   def current_user
     if session[:user_id]
-      @current_user = (Provider.find_by(id: session[:user_id]) || Shelter.find_by(id: session[:user_id]))
+      @current_user ||= User.find_by(id: session[:user_id])
     end
   end
 
